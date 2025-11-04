c1b7fb9508b22ba1052276a0e8b37f6c5083ffb1
daikon <k245014@kccollege.ac.jp>
2025-11-04 14:52:30 +0900

    改行しただけ

--- project/Source/BossStatus.h
+++ project/Source/BossStatus.h
@@ -9,11 +9,11 @@ private:
 		float normalAttack2 = 50.0f;	//攻撃２のダメージ数値
 		float normalAttack3 = 50.0f;	//攻撃３のダメージ数値
 
-		int maxAttack = 3;				//最大連続攻撃の数
-		float maxHp = 1000;				//最大HP
-		float coolTime = 60.0f;			//一段目の攻撃までの時間
-		float runSpeed = 5.0f;			//ボスの移動速度
-		float maxA = 50.0f;				//次の攻撃の間隔を短くする
+		int maxAttack		= 3;		//最大連続攻撃の数
+		float maxHp			= 1000;		//最大HP
+		float coolTime		= 60.0f;	//一段目の攻撃までの時間
+		float runSpeed		= 5.0f;		//ボスの移動速度
+		float maxA			= 50.0f;	//次の攻撃の間隔を短くする
 	};
 	Status status;
 
