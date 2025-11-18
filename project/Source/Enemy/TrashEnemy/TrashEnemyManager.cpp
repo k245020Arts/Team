ba4f7fb17f40530d6c546fbde636337e25dfc34f
daikon <k245014@kccollege.ac.jp>
2025-11-18 13:12:46 +0900

    フィルターと雑魚敵のマネージャー作成

---
+++ project/Source/Enemy/TrashEnemy/TrashEnemyManager.cpp
@@ -0,0 +1,19 @@
+#include "TrashEnemyManager.h"
+
+TrashEnemyManager::TrashEnemyManager()
+{
+}
+
+TrashEnemyManager::~TrashEnemyManager()
+{
+}
+
+void TrashEnemyManager::Update()
+{
+}
+
+void TrashEnemyManager::Draw()
+{
+}
+
+
---
+++ project/Source/Enemy/TrashEnemy/TrashEnemyManager.h
@@ -0,0 +1,16 @@
+#pragma once
+#include ".././../Library/GameObject.h"
+
+class TrashEnemyManager : public GameObject
+{
+public:
+	TrashEnemyManager();
+	~TrashEnemyManager();
+
+	void Update()override;
+	void Draw()override;
+
+	void CreateEnemy();
+private:
+
+};
