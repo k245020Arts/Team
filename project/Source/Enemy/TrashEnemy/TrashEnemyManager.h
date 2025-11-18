16f9569b9c1b220e68371b1e5650b0ad4994d057
daikon <k245014@kccollege.ac.jp>
2025-11-18 15:14:59 +0900

    敵を見えるようにした

--- project/Source/Enemy/TrashEnemy/TrashEnemyManager.h
+++ project/Source/Enemy/TrashEnemy/TrashEnemyManager.h
@@ -1,5 +1,9 @@
 #pragma once
-#include ".././../Library/GameObject.h"
+#include "../../../Library/GameObject.h"
+#include "../EnemyBase.h"
+#include <list>
+
+class TrashEnemy;
 
 class TrashEnemyManager : public GameObject
 {
@@ -9,8 +13,16 @@ public:
 
 	void Update()override;
 	void Draw()override;
+	/// <summary>
+	/// G‹›“G‚Ì¶¬
+	/// </summary>
+	/// <param name="_pos">À•W</param>
+	/// <param name="enemySpawnCounter">‰½‘Ì¶¬‚·‚é‚©</param>
+	void CreateEnemy(VECTOR3 _pos,float enemySpawnCounter);
+	//“G‚ÌƒŠƒXƒg‚Ìæ“¾
+	std::list<TrashEnemy*>& EnemyList(){ return enemies; }
 
-	void CreateEnemy();
 private:
+	std::list<TrashEnemy*> enemies;
 
 };
