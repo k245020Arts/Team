605f3a487f5947f908ee1ff33f863dcf9180c860
西 結斗 <k245020@kccollege.ac.jp>
2025-11-20 15:01:11 +0900

    stage改修追加

--- project/Source/Enemy/Boss/BossState/Attack/BossSpecialAttack1.cpp
+++ project/Source/Enemy/Boss/BossState/Attack/BossSpecialAttack1.cpp
@@ -82,38 +82,6 @@ void BossSpecialAttack1::Update()
 	
 	
 	BossAttackCollsion();
-	//if (a == 0)
-	//{
-	//	velocity.y = 50;
-	//	a = 1;
-	//}
-	//else if (a == 1)
-	//{
-	//	//b->enemyBaseComponent.anim->Play(ID::B_S_ATTACK1);
-
-	//	counter += 0.005f;
-
-	//	velocity.y -= counter;
-	//	if (b->obj->GetTransform()->position.y <= 450)
-	//	{
-	//		a = 2;
-	//		velocity = 0;
-	//		counter = 0;
-	//		b->obj->GetTransform()->position.y = 450;
-	//		b->enemyBaseComponent.state->ChangeState(ID::B_RUN);
-	//	}
-	//	else
-	//	{
-	//		b->LookPlayer();
-	//		rotation = b->obj->GetTransform()->rotation;
-
-	//		float c = 5.0f;
-	//		velocity.x = c * cosf(-rotation.y - 0.5f * DX_PI_F);
-	//		velocity.z = c * sinf(-rotation.y - 0.5f * DX_PI_F);
-	//	}
-	//}
-	
-	//b->obj->GetTransform()->position += velocity;
 }
 
 void BossSpecialAttack1::Draw()
