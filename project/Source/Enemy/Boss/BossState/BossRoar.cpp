05611d43d855e27e41416ca2aeabdcbc04788dca
西 結斗 <k245020@kccollege.ac.jp>
2025-11-28 21:52:59 +0900

    TrashEnemy召喚取りやめ

--- project/Source/Enemy/Boss/BossState/BossRoar.cpp
+++ project/Source/Enemy/Boss/BossState/BossRoar.cpp
@@ -24,7 +24,7 @@ void BossRoar::Update()
 	Boss* b = GetBase<Boss>();
 	if (b->enemyBaseComponent.anim->AnimEventCan()) {
 		if (first) {
-			b->trashEnemy->CreateEnemy(VECTOR3(0, 0, 0), 1);//
+			//b->trashEnemy->CreateEnemy(VECTOR3(0, 0, 0), 1);//
 			first = false;
 			b->enemyBaseComponent.effect->CreateEffekseer(Transform(VECTOR3(0.0f,100.0f,0.0f),VZero,VOne), b->GetBaseObject(), Effect_ID::BOSS_ROAR, 2.0f);
 		}
