524b31137bb43a695a21812c8aadb79576b29758
daiki <k245014@kccollege.ac.jp>
2026-04-09 01:21:29 +0900

    敵のグループ分けのリスト作成

--- project/Source/Enemy/TrashEnemy/TrashEnemyGroup.cpp
+++ project/Source/Enemy/TrashEnemy/TrashEnemyGroup.cpp
@@ -12,13 +12,38 @@ TrashEnemyGroup::~TrashEnemyGroup()
 
 void TrashEnemyGroup::Update()
 {
+	
 }
 
 void TrashEnemyGroup::SettingGroup(TrashEnemy* _enemy, int _index)
 {
 	if (_index % 2 == 0)
+	{
 		_enemy->SetEnemyType(_enemy->EnemyType::MELEE);
+		meleeEnemies.push_back(_enemy);
+	}
 	else if (hasLeader)
+	{
+		_enemy->SetEnemyType(_enemy->EnemyType::RANGED);
+		rangedEnemies.push_back(_enemy);
+	}
+	else
+	{
+		_enemy->SetEnemyType(_enemy->EnemyType::RANGED_LEADER);
+		rangedEnemies.push_back(_enemy);
+		hasLeader = true;
+	}
+}
+
+void TrashEnemyGroup::SetMeleeEnemy(TrashEnemy* _enemy)
+{
+	_enemy->SetEnemyType(_enemy->EnemyType::RANGED);
+	rangedEnemies.push_back(_enemy);
+}
+
+void TrashEnemyGroup::SetRangedEnemy(TrashEnemy* _enemy)
+{
+	if (hasLeader)
 	{
 		_enemy->SetEnemyType(_enemy->EnemyType::RANGED);
 		rangedEnemies.push_back(_enemy);
