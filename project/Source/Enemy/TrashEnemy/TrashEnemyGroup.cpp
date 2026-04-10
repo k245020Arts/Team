fc6a706576053d0cc0735377224be0af76aae874
daiki <k245014@kccollege.ac.jp>
2026-04-10 16:38:46 +0900

    マネージャ＾からグループへの移植作業

--- project/Source/Enemy/TrashEnemy/TrashEnemyGroup.cpp
+++ project/Source/Enemy/TrashEnemy/TrashEnemyGroup.cpp
@@ -1,9 +1,14 @@
 #include "TrashEnemyGroup.h"
 #include "TrashEnemy.h"
+#include "../../Common/Random.h"
+#include "../../Camera/Camera.h"
 
 TrashEnemyGroup::TrashEnemyGroup()
 {
+	camera = FindGameObjectWithTag<Object3D>("CAMERA_OBJ")->Component()->GetComponent<Camera>();
+
 	hasLeader = false;
+	attackCounter = 0;
 }
 
 TrashEnemyGroup::~TrashEnemyGroup()
@@ -12,6 +17,15 @@ TrashEnemyGroup::~TrashEnemyGroup()
 
 void TrashEnemyGroup::Update()
 {
+	Separation();
+	EnemyDeaad(meleeEnemies);
+	EnemyDeaad(rangedEnemies);
+	//近距離の敵関連
+	for (auto melee : meleeEnemies)
+	{
+		MeleeEnemyAttack(melee);
+		CooperateAttackMove(melee);
+	}
 	
 }
 
@@ -38,7 +52,7 @@ void TrashEnemyGroup::SettingGroup(TrashEnemy* _enemy, int _index)
 void TrashEnemyGroup::SetMeleeEnemy(TrashEnemy* _enemy)
 {
 	_enemy->SetEnemyType(_enemy->EnemyType::RANGED);
-	rangedEnemies.push_back(_enemy);
+	meleeEnemies.push_back(_enemy);
 }
 
 void TrashEnemyGroup::SetRangedEnemy(TrashEnemy* _enemy)
@@ -54,4 +68,170 @@ void TrashEnemyGroup::SetRangedEnemy(TrashEnemy* _enemy)
 		rangedEnemies.push_back(_enemy);
 		hasLeader = true;
 	}
+}
+
+int TrashEnemyGroup::GetEnemySize()const
+{
+	return meleeEnemies.size() + rangedEnemies.size();
+}
+
+void TrashEnemyGroup::EnemyDeaad(std::list<TrashEnemy*>& enemies)
+{
+	for (auto itr = enemies.begin(); itr != enemies.end(); )
+	{
+		//雑魚敵が死んでたらlistから削除する
+		if (!(*itr)->GetActive())
+		{
+			(*itr)->GetEnemyObj()->DestroyMe();
+			//次の要素のイテレータが返る
+			itr = enemies.erase(itr);
+		}
+		else
+			++itr;
+	}
+}
+
+void TrashEnemyGroup::Separation()
+{
+	VECTOR pos1 = { 0,0,0 };
+	VECTOR pos2 = { 0,0,0 };
+	const float E_SIZE = 500;
+
+	//for (auto& itr1 : enemies)
+	//{
+	//	for (auto& itr2 : enemies)
+	//	{
+	//		if (itr1 == itr2)
+	//			continue;
+
+	//		pos1 = itr1->GetPos();
+	//		pos2 = itr2->GetPos();
+	//		VECTOR3 vec = pos1 - pos2;
+	//		VECTOR3 vec2 = pos2 - pos1;
+
+	//		vec.y = 0;
+	//		vec2.y = 0;
+
+	//		//エネミーの分散
+	//		if (vec.Size() <= E_SIZE)
+	//		{
+	//			itr1->AddPos(vec.Normalize());
+	//			itr2->AddPos(vec2.Normalize());
+	//		}
+	//	}
+	//}
+}
+
+int TrashEnemyGroup::GetActiveEnemy()
+{
+	int _counter = 0;
+	float* _hp = 0;
+	for (auto& itr : meleeEnemies)
+	{
+		if (itr->GetHp() > _hp)//Activeでやると死んでるモーション挟んでる敵もカウントされるため
+			_counter++;
+	}
+	for (auto& itr : rangedEnemies)
+	{
+		if (itr->GetHp() > _hp)
+			_counter++;
+	}
+
+	return _counter;
+}
+
+void TrashEnemyGroup::MeleeEnemyAttack(TrashEnemy* _enemy)
+{
+	if (_enemy->IsCooperateAtk() || !_enemy->IsAttack())
+		return;
+
+	attackCounter+=Time::DeltaTimeRate();
+
+	if (attackCounter >= ATK_COUNTER_MIN + maxAttackCounter)
+	{
+		if (_enemy->IsAttack())
+		{
+			_enemy->AttackCommand();
+			attackCounter = 0;
+			maxAttackCounter = ATK_COUNTER_MAX * (float)Random::GetReal();
+		}
+	}
+	else if (attackCounter >= ATK_COUNTER_MAX)
+		_enemy->AttackCoolTimeReset();
+}
+
+void TrashEnemyGroup::CooperateAttackMove(TrashEnemy* _enemy)
+{
+	if (!_enemy->IsCooperateAtk())
+		return;
+	int enemiesMax = (int)meleeEnemies.size();
+
+	//連携攻撃のときにその敵が準備完了したかどうか
+	if (_enemy->GetStandby())
+		standbyCounter++;
+	if (standbyCounter >= 1)
+		cooperateCounter += Time::DeltaTimeRate();
+
+	//敵全員が準備完了するか時間経過で攻撃に移る
+	if (standbyCounter == enemiesMax || cooperateCounter >= 3)
+	{
+		AllChangeState(StateID::T_ENEMY_RUN_S);
+		standbyCounter = 0;
+		cooperateCounter = 0;
+	}
+}
+void TrashEnemyGroup::AllChangeState(StateID::State_ID _id)
+{
+	if (GetActiveEnemy() == 0)
+		return;
+
+	for (auto& itr : meleeEnemies)
+	{
+		if (itr->IsMovingToPlayer())
+			itr->ChangeState(_id);
+		else
+			itr->CooperateAtkFinish();
+	}
+}
+void TrashEnemyGroup::CloseWayPoint(std::vector<WayPoint> wayPoint)
+{
+	VECTOR3 position = camera->GetCameraTransform()->position;
+	position.y = 0;
+	//正面べく
+	VECTOR3 frontVec = VECTOR3(0, 0, 1) * MGetRotY(camera->GetCameraTransform()->rotation.y);
+	float counter = 0;
+	VECTOR3 savePos = INFINITY;
+	for (auto enemy : meleeEnemies)
+	{
+		for (auto& itr : wayPoint)
+		{
+			//使っていいウェイポイントを一回だけ探す
+			if (counter == 0)
+			{
+				VECTOR3 vec = itr.position - position;
+				//内積
+				float dotProduct = VDot(frontVec, vec.Normalize());
+				//カメラに写ってるか
+				if (dotProduct > cosf(45 * DegToRad))
+					itr.active = true;
+				//カメラに写ってなかったら
+				else
+					itr.active = false;
+			}
+			//一番近いウェイポイントを探す
+			if (itr.active)
+			{
+				VECTOR3 vec = itr.position - enemy->GetPos();
+				if (savePos.Size() > vec.Size())
+					savePos = itr.position;
+			}
+		}
+		enemy->GetWayPoint(savePos, StateID::COOPERATEATTACK1);
+		counter = 1;
+	}
+}
+
+void TrashEnemyGroup::RangedEnemyAttack(TrashEnemy* _enemy)
+{
+
 }
--- project/Source/Enemy/TrashEnemy/TrashEnemyGroup.h
+++ project/Source/Enemy/TrashEnemy/TrashEnemyGroup.h
@@ -1,8 +1,10 @@
 #pragma once
 #include "../../../Library/GameObject.h"
 #include <list>
+#include "TrashEnemyManager.h"
 
 class TrashEnemy;
+class Camera;
 
 class TrashEnemyGroup : public GameObject
 {
@@ -22,9 +24,46 @@ public:
 	void SetMeleeEnemy(TrashEnemy* _enemy);
 	//‰“‹——£‚Ì“G‚¾‚¯‚ğì‚é
 	void SetRangedEnemy(TrashEnemy* _enemy);
+	//¡¶‚«‚Ä‚é‚·‚×‚Ä‚Ì“G‚Ì”
+	int GetEnemySize()const;
+
+	//ˆê”Ô‹ß‚¢ƒEƒFƒCƒ|ƒCƒ“ƒg‚ğŒvZ‚·‚é
+	void CloseWayPoint(std::vector<WayPoint> wayPoint);
 
 public:
+	Camera* camera;
+	//‹ß‹——£‚Ì“G
 	std::list<TrashEnemy*> meleeEnemies;
+	//‰“‹——£‚Ì“G
 	std::list<TrashEnemy*> rangedEnemies;
+
+	const int ATK_COUNTER_MIN = 1;
+	const float ATK_COUNTER_MAX = 3;
+	
+	void EnemyDeaad(std::list<TrashEnemy*>& enemies);
+	//G‹›“I‚Ç‚¤‚µ‚Å‚ ‚½‚Á‚½‚Ì‰Ÿ‚µ•Ô‚µ
+	void Separation();
+	//¶‚«‚Ä‚é“G‚ğ”‚¦‚éŠÖ”
+	int GetActiveEnemy();
+
 	bool hasLeader;
+
+	//‹ß‹——£‚Ì“G‚Åg‚¤----------
+	//“G‚ÌUŒ‚
+	void MeleeEnemyAttack(TrashEnemy* _enemy);
+	//‹ß‹——£‚Ì“G‚Ì˜AŒgUŒ‚
+	void CooperateAttackMove(TrashEnemy* _enemy);
+	//‹ß‹——£‚Ì“G‚ÌƒXƒe[ƒg‚ğ‹­§“I‚É‘Sˆõ•Ï‚¦‚é
+	void AllChangeState(StateID::State_ID _id);
+	
+	float attackCounter;
+	float maxAttackCounter;
+	float standbyCounter;
+	//˜AŒgUŒ‚‚Å‚±‚ÌƒJƒEƒ“ƒg‚ªˆê’è‚Ì’l‚ğ’´‚¦‚é‚Æ‹­§“I‚ÉUŒ‚‚µn‚ß‚é
+	float cooperateCounter;
+
+	//--------------------------
+
+	//‰“‹——£‚Ì“G‚ÌUŒ‚
+	void RangedEnemyAttack(TrashEnemy* _enemy);
 };
