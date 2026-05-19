f7f2a8402ed48fa39ee6e6a9d477709fd6a72024
daikon <k245014@kccollege.ac.jp>
2026-05-19 15:13:30 +0900

    カメラの制御やUIの追加

--- project/Source/Camera/CameraState/RangedEnemyCamera.cpp
+++ project/Source/Camera/CameraState/RangedEnemyCamera.cpp
@@ -2,6 +2,7 @@
 #include "../Camera.h"
 #include "../../Enemy/TrashEnemy/TrashEnemyManager.h"
 #include "../../Player/player.h"
+#include "../../Common/Easing.h"
 
 RangedEnemyCamera::RangedEnemyCamera()
 {
@@ -10,6 +11,15 @@ RangedEnemyCamera::RangedEnemyCamera()
 
 	enemyPos = VZero;
 	pPos = VZero;
+
+	copyCamPos = VZero;
+
+	angle = 0.0f;
+	a = false;
+
+	isAngleSet = false;
+
+	yButtonImage = LoadGraph("data/image/YButton.png");
 }
 
 RangedEnemyCamera::~RangedEnemyCamera()
@@ -20,23 +30,16 @@ void RangedEnemyCamera::Update()
 {
 	Camera* camera = GetBase<Camera>();
 
-	enemyPos = tEnemyManager->GetRangedLeaderPos();
-	pPos = player->GetPlayerObj()->GetTransform()->position + PlayerPosOffset;
-
-	VECTOR3 dir = VNorm(enemyPos - pPos);
-	dir.y = 0.0f;
-
-	float dist = VSize(enemyPos - pPos);
+	UpdateCamPos(camera);
 
 	camera->target = (pPos + enemyPos) * 0.5f;
-
-	VECTOR3 offset = dir * -1 * CamPos + VGet(0, 400, 0);
-
-	camera->cameraComponent.cameraTransform->position = pPos + offset;
+	UpdeteCamMove(camera);
+	CamRotYMove(camera);
 }
 
 void RangedEnemyCamera::Draw()
 {
+	DrawGraph(YButtonPos.x, YButtonPos.y, yButtonImage,true);
 }
 
 void RangedEnemyCamera::Start()
@@ -48,8 +51,80 @@ void RangedEnemyCamera::Start()
 
 	enemyPos = tEnemyManager->GetRangedLeaderPos();
 	pPos = player->GetPlayerObj()->GetTransform()->position + PlayerPosOffset;
+
+	isAngleSet = false;
 }
 
 void RangedEnemyCamera::Finish()
 {
+}
+
+void RangedEnemyCamera::UpdateCamPos(Camera* _camera)
+{
+	enemyPos = tEnemyManager->GetRangedLeaderPos();
+	pPos = player->GetPlayerObj()->GetTransform()->position + PlayerPosOffset;
+
+	VECTOR3 dir = VNorm(enemyPos - pPos);
+	dir.y = 0.0f;
+
+	float dist = VSize(enemyPos - pPos);
+
+	VECTOR3 offset = dir * -1 * CamPos + VGet(0, 400, 0);
+
+	copyCamPos = pPos + offset;
+}
+
+void RangedEnemyCamera::UpdeteCamMove(Camera* _camera)
+{
+	const float CamSpeed = 0.1f;
+	if (a)
+	{
+		_camera->cameraComponent.cameraTransform->position = copyCamPos;
+		return;
+	}
+
+	//指定したポジションのangleをだす
+	VECTOR3 targetoffset = copyCamPos - pPos;
+	float targetAngle = atan2f(targetoffset.z, targetoffset.x);
+
+	VECTOR3 offset = _camera->cameraComponent.cameraTransform->position - pPos;
+
+	if (!isAngleSet)
+	{
+		angle = atan2f(offset.z, offset.x);
+		isAngleSet = true;
+	}
+	
+	float diff = targetAngle - angle;
+
+	while (diff > DX_PI_F)  diff -= DX_TWO_PI_F;
+	while (diff < -DX_PI_F) diff += DX_TWO_PI_F;
+
+	if (fabsf(diff) < 0.03f)
+	{
+		angle = targetAngle;
+		a = true;
+		return;
+	}
+	else
+		angle += diff * CamSpeed;
+
+	float radius = VSize(targetoffset);
+	_camera->cameraComponent.cameraTransform->position.x = pPos.x + cosf(angle) * radius;
+	_camera->cameraComponent.cameraTransform->position.z = pPos.z + sinf(angle) * radius;
+}
+
+void RangedEnemyCamera::CamRotYMove(Camera* _camera)
+{
+	VECTOR3 offset = _camera->cameraComponent.cameraTransform->position - pPos;
+
+	VECTOR3 enemyToPlayer = VNorm(pPos - enemyPos);
+	VECTOR3 cameraForward = VNorm(_camera->target - _camera->cameraComponent.cameraTransform->position);
+	float dot = VDot(enemyToPlayer, cameraForward);
+	float pi = 0;
+
+	if (dot <= 0.0f)
+		pi = DX_PI_F;
+
+	_camera->cameraComponent.cameraTransform->rotation.y = atan2(offset.x, offset.z) + pi;
 }
--- project/Source/Camera/CameraState/RangedEnemyCamera.h
+++ project/Source/Camera/CameraState/RangedEnemyCamera.h
@@ -21,8 +21,24 @@ private:
 
 	//カメラがプレイヤーとどんだけ離れるか
 	const float CamPos = 700.0f;
+	//カメラがどこにいるべきか計算する関数
+	void UpdateCamPos(Camera* _camera);
+	//カメラの挙動
+	void UpdeteCamMove(Camera* _camera);
+	//カメラのY軸の挙動
+	void CamRotYMove(Camera* _camera);
+
 	//少しずらしたいからプレイヤーに補正をかける数字
 	const VECTOR3 PlayerPosOffset = VECTOR3(150.0f, 0.0f, 0.0f);
+
+	const VECTOR3 YButtonPos = VECTOR3(1100.0f, 200.0f, 0.0f);
+
 	VECTOR3 enemyPos;
 	VECTOR3 pPos;
+	VECTOR3 copyCamPos;
+
+	float angle;
+	bool isAngleSet;
+	bool a;
+	int yButtonImage;
 };
