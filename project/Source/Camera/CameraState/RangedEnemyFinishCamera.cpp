2cb5889990f3b0038a0f65b6d826dc0111889293
daiki <k245014@kccollege.ac.jp>
2026-05-31 23:31:38 +0900

    ちょっと追加

--- project/Source/Camera/CameraState/RangedEnemyFinishCamera.cpp
+++ project/Source/Camera/CameraState/RangedEnemyFinishCamera.cpp
@@ -10,10 +10,16 @@ RangedEnemyFinishCamera::~RangedEnemyFinishCamera()
 
 void RangedEnemyFinishCamera::Update()
 {
+	/*
+	あたった敵にいい感じに拡大
+	エフェクトを足す
+	音を追加する
+	*/
 }
 
 void RangedEnemyFinishCamera::Start()
 {
+	Camera* camera = GetBase<Camera>();
 }
 
 void RangedEnemyFinishCamera::Finish()
